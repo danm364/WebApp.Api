9e3958dabd18ed46b465ab9973174c8feafe0d8a
Daniel McGill <danm364@yahoo.com>
2023-07-01 13:32:14 -0400

    first commit

---
+++ Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Api.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; } 
+
+        public int UserId { get; set; }
+
+    }
+}
---
+++ Entities/CartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Api.Entities
+{
+    public class CartItem
+    {
+
+        public int Id { get; set; } 
+
+        public string CartId { get; set; }
+        
+        public int ProductId { get; set; }
+        
+        public int Qty { get; set; }    
+    }
+}
